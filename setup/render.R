356a1faa6a3a41277ccb86ff67ae940ab5941daa
Victors081 <victorsarmiento8@gmail.com>
2025-02-06 00:18:03 -0500

    Fix: Ejecutar quarto_render_move() reporte a reporte

--- setup/render.R
+++ setup/render.R
@@ -3,21 +3,20 @@ source(file = "setup/functions.R")
 
 ## Render Quarto Report
 
-in_files <- list.files(
-  path = "reports/",
-  pattern = ".qmd",
-  full.names = TRUE
+quarto_render_move(
+  input = "reports/01-calidad_de_datos.qmd",
+  output_file = "reports/01-calidad_de_datos.html",
+  output_dir = "reports/html"
 )
-
-out_files <- gsub(
-  pattern = ".qmd",
-  replacement = ".html",
-  x = in_files
-)
-
 
 quarto_render_move(
-  input = in_files,
-  output_file = out_files,
+  input = "reports/02-analisis_exploratorio_de_datos.qmd",
+  output_file = "reports/02-analisis_exploratorio_de_datos.html",
+  output_dir = "reports/html"
+)
+
+quarto_render_move(
+  input = "reports/03-analisis_econometrico.qmd",
+  output_file = "reports/03-analisis_econometrico.html",
   output_dir = "reports/html"
 )
